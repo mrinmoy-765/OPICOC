6250bf476c125c9d0e3ad23dd857f4eef8c26e4c
Mrinmoy <mrinmoyd629@gmail.com>
2025-12-05 01:35:50 +0600

    card 1

---
+++ frontend/src/components/Home/SingleHomeCards.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card1 from "./SingleCards/Card1";
+import Card2 from "./SingleCards/Card2";
+import Card3 from "./SingleCards/Card3";
+const SingleHomeCards = () => {
+  return (
+    <>
+      <Card1></Card1>
+      <Card2></Card2>
+      <Card3></Card3>
+    </>
+  );
+};
+
+export default SingleHomeCards;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Home/Banner";
+import SingleHomeCards from "../components/Home/SingleHomeCards";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
+      <SingleHomeCards></SingleHomeCards>
     </>
   );
 };
