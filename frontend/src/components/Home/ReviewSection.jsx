219f9854c7c8921c7b60690c92f8e1d220264f7f
Mrinmoy <mrinmoyd629@gmail.com>
2026-02-08 14:03:12 +0600

    review section fix

--- frontend/src/components/Home/ReviewSection.jsx
+++ frontend/src/components/Home/ReviewSection.jsx
@@ -49,26 +49,31 @@ const ReviewSection = () => {
     }
   };
 
-  // MOBILE-FIRST SETTINGS
+  // RESPONSIVE SETTINGS
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     arrows: true,
-    mobileFirst: true,
-    slidesToShow: 1,
+    slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 640,
+        breakpoint: 1024,
         settings: {
           slidesToShow: 2,
         },
       },
       {
-        breakpoint: 1024,
+        breakpoint: 768,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 1.5,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
         },
       },
     ],
